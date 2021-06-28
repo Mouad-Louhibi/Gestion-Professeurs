2a9ff67165fb84ef9b45f4cb500d5342bb628e2c
mouadlouhibi2000@gmail.com <Math-2019>
2021-06-28 23:22:24 +0100

    Add Project Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { ListProfComponent } from './components/list-prof/list-prof.component';
 import { EditProfComponent } from './components/edit-prof/edit-prof.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { ListProjectComponent } from './components/list-project/list-project.component';
 
 const routes: Routes = [
   {path:'add-prof',component:ProfFormComponent},
   {path:'list-prof',component:ListProfComponent},
   {path:'edit-prof',component:EditProfComponent},
+  {path:'add-project',component:AddProjectComponent},
+  {path:'list-project',component:ListProjectComponent},
   {path:'login',component:LoginComponent},
   {path:'', component:DashboardComponent},
   // {path:"**",component:ErrorComponent},
--- src/app/components/add-project/add-project.component.ts
+++ src/app/components/add-project/add-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-project',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-project.component.scss']
 })
 export class AddProjectComponent implements OnInit {
+  
+  public form: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      nom: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      prenom: new FormControl(''),
+      etat: new FormControl('')
+    })
   }
 
 }
